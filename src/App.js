444cfac4d565d59851d0a1dc7c5337d23d54f3dd
Liang Guo <git@lkuo.me>
2019-02-03 22:32:21 -0500

    LG - add router and sample page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {Link, Route} from "react-router-dom";
+import {Home} from "./home/Home";
+import {Login} from "./login/Login";
+import {Register} from "./register/Register";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <nav>
+          <Link to='/'>Home</Link>
+          <Link to='login'>Login</Link>
+          <Link to='register'>Register</Link>
+        </nav>
+        <div>
+          <Route exact path='/' component={Home}/>
+          <Route path='/login' component={Login}/>
+          <Route path='/register' component={Register}/>
+        </div>
       </div>
     );
   }
---
+++ src/register/Register.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Register = () => (
+  <div>
+    <h1>Register</h1>
+  </div>
+);
